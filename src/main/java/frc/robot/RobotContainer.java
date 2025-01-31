85e52660f234f2c039180a9608aa5d203888ac26
wheaton01 <jacob.wheaton@oakland.k12.mi.us>
2025-01-30 20:41:02 -0500

    more drive pose stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,80 +1,89 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.swerveConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.climberCommands.setClimber;
 import frc.robot.commands.intakeCommands.setIntake;
+import frc.robot.commands.SwerveCommands.DriveToPoseCommand;
 import frc.robot.subsystems.sClimber;
 import frc.robot.subsystems.sElevator;
 import frc.robot.subsystems.sEndAffector;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-
 public class RobotContainer {
-  sEndAffector sEndAffector;
-  sClimber sClimber;
-  sElevator sElevator;
+    sEndAffector sEndAffector;
+    sClimber sClimber;
+    sElevator sElevator;
+    SwerveSubsystem swerveSubsystem;
 
-  setIntake defaultIntake;
-  setClimber defaultClimber;
-  
-  
+    setIntake defaultIntake;
+    setClimber defaultClimber;
 
-  //private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    private final CommandXboxController m_driverController =
+        new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
-  // Initialize sEndAffector in the constructor
+    public final CommandXboxController m_operatorController =
+        new CommandXboxController(OperatorConstants.kOperatorControllerPort);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    public RobotContainer() {
+        sEndAffector = new sEndAffector();
+        sClimber = new sClimber();
+        sElevator = new sElevator();
+        swerveSubsystem = new SwerveSubsystem(new File("path/to/config"));
 
-  public final CommandXboxController m_operatorController =
-      new CommandXboxController(OperatorConstants.kOperatorControllerPort);
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Initializing Subsystems
-    sEndAffector = new sEndAffector();
-    sClimber =     new sClimber();
-    sElevator =    new sElevator();
-
-    setupCommands();
-    
-        // Configure the trigger bindings
+        setupCommands();
         configureBindings();
-      }
-    
-    
-      private void setupCommands() {
-         defaultIntake = new setIntake(false, false, 0, sEndAffector);
-         defaultClimber =new setClimber(sClimber, false, false);
+    }
 
+    private void setupCommands() {
+        defaultIntake = new setIntake(false, false, 0, sEndAffector);
+        defaultClimber = new setClimber(sClimber, false, false);
 
-         sEndAffector.setDefaultCommand(defaultIntake);
-         sClimber.setDefaultCommand(defaultClimber);
-         //sElevator.setDefaultCommand(defaultElevator);
-      }
-    
-    
-      private void configureBindings() {
+        sEndAffector.setDefaultCommand(defaultIntake);
+        sClimber.setDefaultCommand(defaultClimber);
+    }
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    
-    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
-  
+    private void configureBindings() {
+        // Bind left stick to standard swerve drive
+        swerveSubsystem.setDefaultCommand(
+            swerveSubsystem.driveCommand(
+                m_driverController::getLeftX,
+                m_driverController::getLeftY,
+                m_driverController::getRightX
+            )
+        );
 
+        // Bind right trigger to drive to nearest AprilTag pose to the right
+        new Trigger(() -> m_driverController.getRightTriggerAxis() > 0.5)
+            .whileTrue(new DriveToPoseCommand(
+                swerveSubsystem,
+                swerveConstants.MAX_SPEED,
+                m_driverController::getLeftX, // xOffset
+                m_driverController::getLeftY, // yOffset
+                m_driverController::getRightX, // rotationOffset
+                m_driverController::getLeftTriggerAxis,
+                m_driverController::getRightTriggerAxis
+            ));
 
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    //todo : this is all temporary
-    return Autos.getAutonomousCommand(sEndAffector);
-  }
+        // Bind left trigger to drive to nearest AprilTag pose to the left
+        new Trigger(() -> m_driverController.getLeftTriggerAxis() > 0.5)
+            .whileTrue(new DriveToPoseCommand(
+                swerveSubsystem,
+                swerveConstants.MAX_SPEED,
+                m_driverController::getLeftX, // xOffset
+                m_driverController::getLeftY, // yOffset
+                m_driverController::getRightX, // rotationOffset
+                m_driverController::getLeftTriggerAxis,
+                m_driverController::getRightTriggerAxis
+            ));
+    }
+
+    public Command getAutonomousCommand() {
+        return Autos.getAutonomousCommand(sEndAffector);
+    }
 }
