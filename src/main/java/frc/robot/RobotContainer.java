8e2a1d6428b61820bc14f29a6029362573c0d58b
Wheaton <jacob.wheaton@oakland.k12.mi.us>
2025-02-12 21:25:36 -0500

    adding most elevator commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,8 +2,11 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.swerveConstants;
+import frc.robot.Constants.robotConstants.elevatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.setCHaptics;
 import frc.robot.commands.climberCommands.setClimber;
+import frc.robot.commands.elevatorCommands.setElevatorPose;
 import frc.robot.commands.intakeCommands.setIntake;
 import frc.robot.commands.SwerveCommands.DriveToPoseCommand;
 import frc.robot.subsystems.sClimber;
@@ -15,8 +18,10 @@ import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 import java.io.File;
 
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -29,6 +34,8 @@ public class RobotContainer {
     setIntake defaultIntake, intakeBall;
     setClimber defaultClimber;
     sControllerHaptics m_controllerHaptics;
+    setElevatorPose setL4Pose, setL3Pose,setL2Pose,setL1Pose, setHomePose;
+
 
     private final CommandXboxController m_driverController =
         new CommandXboxController(OperatorConstants.kDriverControllerPort);
@@ -55,7 +62,12 @@ public class RobotContainer {
         defaultClimber = new setClimber(sClimber, false, false);
         intakeBall = new setIntake(false, true, 0.0,-1.0, sEndAffector);
 
-
+        //Elevator Commands
+        setL4Pose = new   setElevatorPose(sElevator, elevatorConstants.kL4Height);
+        setL3Pose = new   setElevatorPose(sElevator, elevatorConstants.kL3Height);
+        setL2Pose = new   setElevatorPose(sElevator,elevatorConstants.kL2Height);
+        setL1Pose = new   setElevatorPose(sElevator,elevatorConstants.kL1Height);
+        setHomePose = new setElevatorPose(sElevator,elevatorConstants.kHomePose);
 
 
         //Setting Default Commands
@@ -64,7 +76,10 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-        // Bind left stick to standard swerve drive
+        driverControls();
+        operatorControls();
+    }
+    public void driverControls(){
         swerveSubsystem.setDefaultCommand(
             swerveSubsystem.driveCommand(
                 m_driverController::getLeftX,
@@ -126,9 +141,54 @@ public class RobotContainer {
                             false // Set to false for non-autonomous mode, change as needed
                         ));
                         
-                    
+      
 
     }
+
+    public void operatorControls() {
+        // ------------------------- Preset Pose Commands ------------------------- //
+        new Trigger(m_operatorController.a()).onTrue(setL1Pose);
+        new Trigger(m_operatorController.b()).onTrue(setL3Pose);
+        new Trigger(m_operatorController.x()).onTrue(setL2Pose);
+        new Trigger(m_operatorController.a()).onTrue(setHomePose); // Note: Duplicated A button binding?
+    
+        // ----------------------- Climber Safety Override ----------------------- //
+        new Trigger(m_operatorController.rightStick()
+            .and(m_operatorController.leftStick()))
+            .onTrue(new ParallelCommandGroup(
+                new setCHaptics(m_controllerHaptics, 0.8).withTimeout(1.2),
+                new InstantCommand(sClimber::disableSafety)
+            ));
+    
+        // --------------------------- Intake Controls --------------------------- //
+    
+        // Deploy intake when Left Bumper is pressed, retract when released
+        new Trigger(m_operatorController.leftBumper().negate())
+            .onTrue(new setIntake(false, true, 0, 0, sEndAffector))   // Deploy
+            .onFalse(new setIntake(false, false, 0, 0, sEndAffector)); // Retract
+    
+        // Run intake when Left Trigger is pressed beyond 0.2 threshold
+        new Trigger(m_operatorController.leftTrigger(0.2))
+            .onTrue(new setIntake(false, true, 1.0, 0, sEndAffector))  // Intake game piece
+            .onFalse(new setIntake(false, true, 0, 0, sEndAffector));  // Stop intake
+    
+        // Eject game piece when Right Trigger is pressed beyond 0.2 threshold
+        new Trigger(m_operatorController.rightTrigger(0.2))
+            .onTrue(new setIntake(false, true, -1.0, 0, sEndAffector)) // Reverse intake (Eject)
+            .onFalse(new setIntake(false, true, 0, 0, sEndAffector));  // Stop intake
+    
+        // Enable Coral Sensor for automatic intake when Right Bumper is pressed
+        new Trigger(m_operatorController.rightBumper())
+            .onTrue(new setIntake(true, true, 1.0, 0, sEndAffector))  // Auto intake using Coral Sensor
+            .onFalse(new setIntake(true, true, 0, 0, sEndAffector));  // Stop intake
+    
+        // Placing Mode (Y Button) - Runs the placing motor
+        new Trigger(m_operatorController.y())
+            .onTrue(new setIntake(false, true, 0, 1.0, sEndAffector)) // Start placing motor
+            .onFalse(new setIntake(false, true, 0, 0, sEndAffector)); // Stop placing motor
+    }
+    
+    
 
     public Command getAutonomousCommand() {
         return Autos.getAutonomousCommand(sEndAffector);
---
+++ src/main/java/frc/robot/commands/setCHaptics.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.sControllerHaptics;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class setCHaptics extends Command {
+  /** Creates a new setCHaptics. */
+  sControllerHaptics sControllerHaptics;
+  double setpoint;
+  public setCHaptics(sControllerHaptics sControllerHaptics, double setpoint) {
+    this.sControllerHaptics = sControllerHaptics;
+    this.setpoint = setpoint;
+    addRequirements(sControllerHaptics);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    sControllerHaptics.setHaptics(setpoint);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    sControllerHaptics.setHaptics(0);
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
